c713ba31ff426c858acefefd653c858521334f9f
pramu-k <pramuditha.kaluthanthri@churchillcap.com>
2024-08-18 18:36:14 +0530

    Order detail and order header models

--- Bulky.Models/OrderDetail.cs
+++ Bulky.Models/OrderDetail.cs
@@ -9,20 +9,19 @@ using System.Threading.Tasks;
 
 namespace Bulky.Models
 {
-    public class ShoppingCart
+    public class OrderDetail
     {
         public int Id { get; set; }
+        public int OrderHeaderId { get; set; }
+        [ForeignKey(nameof(OrderHeaderId))]
+        [ValidateNever]
+        public OrderHeader OrderHeader { get; set; }
+        [Required]
         public int ProductId { get; set; }
-        [ForeignKey("ProductId")]
+        [ForeignKey(nameof(ProductId))]
         [ValidateNever]
         public Product Product { get; set; }
-        [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
         public int Count { get; set; }
-        public string ApplicationUserId { get; set; }
-        [ForeignKey("ApplicationUserId")]
-        [ValidateNever]
-        public ApplicationUser ApplicationUser { get; set; }
-        [NotMapped]
         public double Price { get; set; }
     }
 }
